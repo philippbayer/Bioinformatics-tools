2392ab271a7e33b48c39e6d01cbcb0418753ff23
Philipp Bayer <PhilippBay@gmail.com>
2012-03-16 14:58:32 +1000

    .gb is more common file-ending

--- GenbankToFasta.py
+++ GenbankToFasta.py
@@ -21,15 +21,15 @@ parser.add_argument('-d', help='The directory in which the Genbank-files are loc
 args = parser.parse_args()
 
 # go through all elements of the directory given,
-# convert the ones ending in .gbk
+# convert the ones ending in .gb
 counter = 0
 for element in os.listdir(vars(args)['d']):
     # if the lower-case version of an element ends in
-    # .gbk it's probably a Genbank-file
+    # .gb it's probably a Genbank-file
     if element.lower().endswith(".gb"):
         # open input/output-files
         file_handle = open(element)
-        output_handle =  open(element.replace(".gbk", ".fasta"), "w")
+        output_handle =  open(element.replace(".gb", ".fasta"), "w")
         # parse input and write to output
         for seq_record in SeqIO.parse(file_handle, "genbank"):
             output_handle.write(">%s %s\n%s\n" % (
